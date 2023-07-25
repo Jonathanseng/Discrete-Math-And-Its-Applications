5cdef20ca3a77100edaf8be9cbdbb2770a50e6f9
corodostudio@gmail.com <corodostudio@gmail.com>
2023-07-25 12:48:45 +0700

    set introduction

---
+++ introduction to set.py
@@ -0,0 +1,43 @@
+In mathematics, a set is a well-defined collection of distinct objects. The objects in a set are called its elements or members. Sets can be represented in a variety of ways, including roster form, set-builder form, and Venn diagrams.
+
+* **Roster form** is the most common way to represent a set. It uses curly braces to enclose the elements of the set, separated by commas. For example, the set of even numbers less than 10 can be represented as {2, 4, 6, 8}.
+* **Set-builder form** uses a colon and a variable to specify the elements of a set. For example, the set of even numbers less than 10 can also be represented as {x | x is an even number and 0 < x < 10}.
+* **Venn diagrams** are a graphical way to represent sets. They use circles to represent sets, and the overlaps between the circles represent the elements that are in more than one set.
+
+Sets can be classified into different types, including:
+
+* **Finite sets** have a finite number of elements. For example, the set of even numbers less than 10 is a finite set.
+* **Infinite sets** have an infinite number of elements. For example, the set of all natural numbers is an infinite set.
+* **Equal sets** have exactly the same elements. For example, the set of even numbers less than 10 and the set of integers between 0 and 9 are equal sets.
+* **Subsets** are sets that are contained within another set. For example, the set of even numbers less than 10 is a subset of the set of all natural numbers.
+
+Sets are a fundamental concept in mathematics, and they are used in a wide variety of mathematical applications. For example, sets are used to define functions, to prove mathematical theorems, and to solve problems in logic.
+
+
+Set notation is a way of representing sets using symbols. It is a concise and precise way to define sets, and it is used in many areas of mathematics, including set theory, logic, and computer science.
+
+There are two main types of set notation: roster form and set-builder form.
+
+* **Roster form** uses curly braces to enclose the elements of a set, separated by commas. For example, the set of even numbers less than 10 can be represented as {2, 4, 6, 8}.
+* **Set-builder form** uses a colon and a variable to specify the elements of a set. For example, the set of even numbers less than 10 can also be represented as {x | x is an even number and 0 < x < 10}.
+
+Here are some other symbols that are commonly used in set notation:
+
+* **$\cup$** denotes the union of two sets. For example, the union of the set of even numbers less than 10 and the set of odd numbers less than 10 is the set of all integers less than 10.
+* **$\cap$** denotes the intersection of two sets. For example, the intersection of the set of even numbers less than 10 and the set of odd numbers less than 10 is the empty set.
+* **$\backslash$** denotes the difference of two sets. For example, the difference of the set of even numbers less than 10 and the set of odd numbers less than 10 is the set of even numbers that are divisible by 4.
+* **$\subseteq$** denotes that a set is a subset of another set. For example, the set of even numbers less than 10 is a subset of the set of all integers less than 10.
+* **$\supseteq$** denotes that a set is a superset of another set. For example, the set of all integers less than 10 is a superset of the set of even numbers less than 10.
+
+Set properties are the rules that govern how sets can be manipulated. They are used to ensure that set operations are performed correctly.
+
+Here are some of the most important set properties:
+
+* **Commutativity**. The order in which sets are combined does not affect the result. For example, the union of the set of even numbers less than 10 and the set of odd numbers less than 10 is the same as the union of the set of odd numbers less than 10 and the set of even numbers less than 10.
+* **Associativity**. The order in which sets are combined does not affect the result, as long as the same operation is performed on each set. For example, the union of the set of even numbers less than 10, the set of odd numbers less than 10, and the set of negative numbers less than 10 is the same as the union of the set of even numbers less than 10, the union of the set of odd numbers less than 10 and the set of negative numbers less than 10, and the set of negative numbers less than 10.
+* **Distributivity**. The union of a set and the intersection of two other sets is the same as the union of the set and the two individual sets. For example, the union of the set of even numbers less than 10 and the intersection of the set of odd numbers less than 10 and the set of negative numbers less than 10 is the same as the union of the set of even numbers less than 10, the set of odd numbers less than 10, and the set of negative numbers less than 10.
+* **Identity**. The union of a set and the empty set is the same as the original set. For example, the union of the set of even numbers less than 10 and the empty set is the same as the set of even numbers less than 10.
+* **Complement**. The complement of a set is the set of all elements that are not in the original set. For example, the complement of the set of even numbers less than 10 is the set of all odd numbers less than 10.
+
+These are just a few of the most important set properties. There are many other set properties that are used in mathematics and computer science.
+
